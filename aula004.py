a49f4a690d277fbb67716319f5829d2759c1a76c
desucasas <deboramilitani@gmail.com>
2025-11-04 18:38:46 -0300

    Retangulo Terreno

---
+++ aula004.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.title("Problema Terreno")
+#Entrada de dados
+st.write("Digite a largura do terreno em metros:")
+largura = st.number_input("Largura (m):")
+st.write("Digite o comprimento do terreno em metros:")
+comprimento = st.number_input("Comprimento (m):")
