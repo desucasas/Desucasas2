fa867589f3bd8a35925c56e2da9b6864f0795ba9
desucasas <deboramilitani@gmail.com>
2025-11-04 19:18:10 -0300

    MedidaST

--- retanguloST.py
+++ retanguloST.py
@@ -1,10 +1,11 @@
 import streamlit as st #Framework
 import math as mt #Biblioteca
 #Problema Retângulo
-st.title("Problema Retângulo")
+TITULO = "Problema Retângulo"
+st.title(TITULO)
 #Entrada de dados
-base = st.number_input("Digite a base do retângulo:")
-altura = st.number_input("Digite a altura do retângulo:")
+base = st.number_input("Digite a base do retângulo:", min_value=0.0, format="%.1f")
+altura = st.number_input("Digite a altura do retângulo:", min_value=0.0, format="%.1f")
 #Processamento de dados
 area = base * altura
 perimetro = 2 * base + altura * 2
@@ -12,6 +13,6 @@ perimetro = 2 * base + altura * 2
 x = mt.pow(base,2) + mt.pow(altura,2)
 diagonal = mt.sqrt(x)
 #Saída de dados
-st.write(f"A área do retângulo é {area}")
-st.write(f"O perímetro do retângulo é {perimetro}")
-st.write(f"A diagonal do retângulo é {diagonal}")
+st.write(f"A área do retângulo é {area:.1f}")
+st.write(f"O perímetro do retângulo é {perimetro:.1f}")
+st.write(f"A diagonal do retângulo é {diagonal:.1f}")
